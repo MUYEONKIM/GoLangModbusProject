1a450850801e32d7f50d816e53c3ef0f212fa6af
MUYEONKIM <rlaandus2@gmail.com>
2025-03-05 11:43:41 +0900

    [feat] connect 루틴과 read 루틴을 병합

--- backup/mainbackup0304.go
+++ backup/mainbackup0304.go
@@ -34,7 +34,6 @@ type Device struct {
 	DeviceInfo *modbus.ModbusClient
 	SucessCount int
 	ErrorCount int
-	Com int
 }
 
 var (
@@ -47,7 +46,7 @@ var (
 
 	// map관리
 	Storage = make(map[string]*maxDataStruct)
-	Device_Clients = make(map[string]*Device)
+	Device_Clinets = make(map[string]*Device)
 	
 	// 채널관리
 	stopReadChan = make(chan bool)
@@ -55,103 +54,56 @@ var (
 	sendMaxDataChan = make(chan maxDataStruct, 1)
 )
 
-func ConnectModbus(DvcId string, client *modbus.ModbusClient) error {
-	if _, exists := Device_Clients[DvcId]; !exists {
-		Device_Clients[DvcId] = &Device{}
-	}
+func ConnectModbus(ip string, port string) (*modbus.ModbusClient, error){
+	
+	// 단순히 client라는 struct을 만들어 주는 것이라서 여기서는 err가 안남
+	client, err = modbus.NewClient(&modbus.ClientConfiguration{
+		// URL: "tcp://" + ip + ":" + "502",
+		URL: "tcp://" + ip + ":" + port,
+		Speed : 1000,
+		Timeout: 1 * time.Second,
+		
+	})
+
 
 	// Open 시 에러가 나는 것이니 에러처리는 아래 쪽
 	err = client.Open()
-
-
 	if err != nil {
-		// fmt.Println("Device", DvcId, " Error : ",err)
-		// 에러카운트 추가 해줘야 할것
-		Device_Clients[DvcId].ErrorCount++
-
-		// time.Sleep(100 * time.Millisecond)
-
-		fmt.Println(Device_Clients[DvcId].ErrorCount)
-		if Device_Clients[DvcId].ErrorCount == 3 {
-			return fmt.Errorf(DvcId, " 통신 실패로 인한 disconnect")
-		}
-
-		return ConnectModbus(DvcId, client)
-	}
-	Device_Clients[DvcId].Com = 1
-	Device_Clients[DvcId].SucessCount++
+		fmt.Println(err)
+		return nil, err
 		// 에러 발생시 에러 카운트 하나 증가 시키고, 1초후 reconnect errcnt가 10을 넘기면 disconnect?
 		// reconnect 되었을 때 read를 하고 있으면 자동으로 read가 되게 해야할 듯
-	return nil
+	}
+	return client, nil
 }
 
 // dvc정보를 하나로 합치고 go routine을 connect와 read를 개별로 두지 말고 일단 하나로 합치자
 
-func Start(Count int, Batchsize int, ip string, length int, port int) error {
+func Start(ip string, length int, port int) error {
 	if length != 0 {
 		for portNum := port; portNum < port + length; portNum++ {
-			wg.Add(1)
-			go func(portNum int){
-				defer wg.Done()
-				DvcId := fmt.Sprintf("Device%v", portNum)
-	
-				if _, exists := Device_Clients[DvcId]; !exists {
-					Device_Clients[DvcId] = &Device{}
-				}
-	
-				client, err = modbus.NewClient(&modbus.ClientConfiguration{
-					// URL: "tcp://" + ip + ":" + "502",
-					URL: "tcp://" + ip + ":" + strconv.Itoa(portNum),
-					Speed : 1000,
-					Timeout: 1 * time.Second,
-				})
+			// 아래 이것들도 각각의 go 함수로 실행이 되어야 함
+			// 각각의 지역변수 Client가 따로 생기는 것
+			// 현재 그러면 device_clients map에는 mutex로 접근이 되야한다
+			// 에러가 뜨더라도 계속 다시 실행을 해보아야 하기 때문에 일단 key값으로는 만들어진다.
+			// 또한 계속 연결을 반복해야 하기 때문에 몇번이상 err cnt이상이면 통신 끊기 이런건 없음
+			// 그리고 com이라는 객체값을 부여하여, 에러가 발생한 상황이면 com이 0 상태, 통신이 성공하면 1 상태로 해준다
+			// 그럼 ConnectModbus 함수를 아래처럼 분리할 필요가 있을까?
+			// 한군데 합치는 걸로 충분히 가능할 듯 
+			Client, err := ConnectModbus(ip, strconv.Itoa(portNum))
+
+			if err != nil {
+				return err
+			}
+
 			
-				Device_Clients[DvcId].DeviceInfo = client
-	
-				// 아래 이것들도 각각의 go 함수로 실행이 되어야 함
-				// 각각의 지역변수 Client가 따로 생기는 것
-				// 현재 그러면 device_clients map에는 mutex로 접근이 되야한다
-				// 에러가 뜨더라도 계속 다시 실행을 해보아야 하기 때문에 일단 key값으로는 만들어진다.
-				// 또한 계속 연결을 반복해야 하기 때문에 몇번이상 err cnt이상이면 통신 끊기 이런건 없음
-				// 그리고 com이라는 객체값을 부여하여, 에러가 발생한 상황이면 com이 0 상태, 통신이 성공하면 1 상태로 해준다
-				// 그럼 ConnectModbus 함수를 아래처럼 분리할 필요가 있을까?
-				// 한군데 합치는 걸로 충분히 가능할 듯 
-	
-				err := ConnectModbus(DvcId, client)
-	
-				if err != nil {
-					for K, v := range Device_Clients {
-						fmt.Printf("%v %+v \n",K, v )
-					}
-					// fmt.Println(err)
-				}
-
-				// id 통일은 아래
-				client.SetUnitId(uint8(1))
-			
-				// slave id 다른건 아래
-				// err := device.SetUnitId(uint8(index + 1))
-				wg.Add(1)
-
-				ticker := time.NewTicker(300 * time.Millisecond)
-
-				// 일단은 Com으로 해놓았지만 select case로 연결되었을 때 안되었을 때를 지정해줘야 한다
-				if Device_Clients[DvcId].Com == 1 {
-					ReadModbus(client, DvcId, 1300, 125, &wg ,Count, ticker)
-				}
-			}(portNum)
 			// readtest가 여기에 들어가야 할 것이다
-			// Device_Client = append(Device_Client, Client)
-			// fmt.Printf("%+v \n",Client)
+			Device_Client = append(Device_Client, Client)
+			fmt.Printf("%+v \n",Client)
 		}
 		// 여기에 save가 들어와야 겠죠?
-		// for _, v := range Device_Clinets {
-		// 	fmt.Printf("%+v \n", v)
-		// }
-		// fmt.Printf("%+v \n", Device_Clinets)
-
-		// fmt.Println(len(Device_Clinets), "개의 디바이스가 연결되었습니다")
-	}
+		fmt.Println(len(Device_Client), "개의 디바이스가 연결되었습니다")
+		}
 	return fmt.Errorf("최소 1개이상의 디바이스를 연결해야 합니다.")
 }
 
@@ -167,103 +119,6 @@ func DisconnectModbus(ip string) error {
 	Device_Client = []*modbus.ModbusClient{}
 	return nil
 
-}
-
-// max값 판별 함수 
-func MaxOfSensorData(reg64s []uint16, currentTime time.Time, maxData maxDataStruct) maxDataStruct {
-	if len(reg64s) == 0 {
-		log.Fatal("No data")
-		fmt.Println("No data")
-	}
-
-	if len(maxData.maxValues) == 0 {
-		maxData.maxValues = reg64s
-		maxData.maxTimestamp = currentTime
-	}
-	// maxValues가 담기는 이유는, 슬라이스라서 그렇다. 슬라이스는 pass by value로 되더라도, 가리키는 배열의 주소가 복사되므로, 자동으로 pass by pointer가 되는데
-	// maxTimestamp는 time.Time이라서 pass by value로 되어서, 값이 복사되어서 들어가게 된다. 따라서 기존의 값이 변경되는 것이 아님 
-
-	for i, value := range reg64s {
-		if maxData.maxValues[i] < value{
-			maxData.maxValues[i] = value
-			maxData.maxTimestamp = currentTime
-		}
-	}
-
-	return maxData
-}
-
-func ReadModbus(client *modbus.ModbusClient, DvcId string,addr, quantity uint16, wg *sync.WaitGroup, count int,  ticker *time.Ticker) {
-	var maxData = maxDataStruct{}
-
-	defer ticker.Stop()
-	defer wg.Done()
-	MaxCount := 0
-	cnt := 0
-	wg.Add(1)
-	// 0.3초마다 storage에 데이터 쌓이는 루틴
-	go func() {
-		defer wg.Done()
-
-		for {
-			select {
-			case result := <- sendMaxDataChan:
-				maxData = result
-			case <- ticker.C:
-				copyData := maxData
-
-				mu.Lock()
-				if _, exist := Storage[DvcId]; !exist {
-					// 주소를 저장 (새롭게 생성)
-					Storage[DvcId] = &copyData
-				} else {
-					// 저장된 주소값에 역참조로 데이터를 덮어씌움 (업데이트)
-					*(Storage[DvcId]) = copyData
-				}
-				mu.Unlock()
-
-				cnt++
-				maxData.maxValues = make([]uint16, 0)
-				maxData.maxTimestamp = time.Time{}
-			}
-		}
-	}()
-	// 0.5초마다 max값 판별
-	for {
-		select {
-		case <- stopReadChan:
-			fmt.Println("Stop read")
-			return
-		default:
-			if MaxCount == count {
-				mu.Lock()
-				fmt.Printf("%+v \n", testCount)
-				stopReadChan <- true
-				wg.Done()
-				mu.Unlock()
-				return
-			}
-			var reg64s []uint16
-
-			reg64s, err = client.ReadRegisters(addr - 1,quantity,modbus.HOLDING_REGISTER)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			fmt.Println(DvcId, reg64s)
-			currentTime := time.Now()
-			var maxDatas = MaxOfSensorData(reg64s, currentTime, maxData)
-			sendMaxDataChan <- maxDatas
-			mu.Lock()
-			testCount[DvcId]++
-			mu.Unlock()
-
-			MaxCount++
-
-			// 0.05초 대기
-			time.Sleep(50 * time.Millisecond)
-		}
-	}
 }
 
 
@@ -420,18 +275,121 @@ func main() {
 		}
 	}
 
-	
+	// max값 판별 함수 
+	MaxOfSensorData := func(reg64s []uint16, currentTime time.Time, maxData maxDataStruct) maxDataStruct {
+		if len(reg64s) == 0 {
+			log.Fatal("No data")
+			fmt.Println("No data")
+		}
+
+		if len(maxData.maxValues) == 0 {
+			maxData.maxValues = reg64s
+			maxData.maxTimestamp = currentTime
+		}
+		// maxValues가 담기는 이유는, 슬라이스라서 그렇다. 슬라이스는 pass by value로 되더라도, 가리키는 배열의 주소가 복사되므로, 자동으로 pass by pointer가 되는데
+		// maxTimestamp는 time.Time이라서 pass by value로 되어서, 값이 복사되어서 들어가게 된다. 따라서 기존의 값이 변경되는 것이 아님 
+
+		for i, value := range reg64s {
+			if maxData.maxValues[i] < value{
+				maxData.maxValues[i] = value
+				maxData.maxTimestamp = currentTime
+			}
+		}
+
+		return maxData
+	}
 
 	// 모드버스 통신 값을 읽고 interval 마다 Storage buffer에 데이터를 저장시켜주는 역할
+	ReadModbus := func (index int, device *modbus.ModbusClient,addr, quantity uint16, wg *sync.WaitGroup, count, batchsize int, ticker *time.Ticker) {
+		dvcid := fmt.Sprintf("D0000000%v", index + 1)
+		var maxData = maxDataStruct{}
+
+		defer ticker.Stop()
+		defer wg.Done()
+		MaxCount := 0
+		cnt := 0
+		wg.Add(1)
+		// 0.3초마다 storage에 데이터 쌓이는 루틴
+		go func() {
+			defer wg.Done()
+
+			for {
+				select {
+				case result := <- sendMaxDataChan:
+					maxData = result
+				case <- ticker.C:
+					copyData := maxData
+
+					mu.Lock()
+					if _, exist := Storage[dvcid]; !exist {
+						// 주소를 저장 (새롭게 생성)
+						Storage[dvcid] = &copyData
+					} else {
+						// 저장된 주소값에 역참조로 데이터를 덮어씌움 (업데이트)
+						*(Storage[dvcid]) = copyData
+					}
+					mu.Unlock()
+					cnt++
+					maxData.maxValues = make([]uint16, 0)
+					maxData.maxTimestamp = time.Time{}
+				}
+			}
+		}()
+		// 0.5초마다 max값 판별
+		for {
+			select {
+			case <- stopReadChan:
+				fmt.Println("Stop read")
+				return
+			default:
+				if MaxCount == count {
+					mu.Lock()
+					fmt.Printf("%+v \n", testCount)
+					stopReadChan <- true
+					wg.Done()
+					mu.Unlock()
+					return
+				}
+				var reg64s []uint16
+
+				// id 통일은 아래
+				err := device.SetUnitId(uint8(1))
+				
+				// slave id 다른건 아래
+				// err := device.SetUnitId(uint8(index + 1))
+
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+
+				reg64s, err = device.ReadRegisters(addr - 1,quantity,modbus.HOLDING_REGISTER)
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+
+				currentTime := time.Now()
+				var maxDatas = MaxOfSensorData(reg64s, currentTime, maxData)
+				sendMaxDataChan <- maxDatas
+				fieldName := "DV00" + strconv.Itoa(index)
+				mu.Lock()
+				testCount[fieldName]++
+				mu.Unlock()
 	
+				MaxCount++
+
+				// 0.05초 대기
+				time.Sleep(50 * time.Millisecond)
+			}
+		}
+	}
 
 	app.Post("/", func(c fiber.Ctx) error {
 		type readBody struct {
 			Dvc_len int
 			Ip string
 			Port int
-			Count int
-			Batchsize int
 		}
 
 		var result readBody
@@ -443,7 +401,7 @@ func main() {
 			return c.Status(fiber.StatusBadRequest).SendString("Invalid request body")
 		}
 
-		err = Start(result.Count, result.Batchsize, result.Ip, result.Dvc_len, result.Port)
+		err = Start(result.Ip, result.Dvc_len, result.Port)
 
 		if err != nil {
 			return c.Status(fiber.StatusBadRequest).SendString(err.Error())
@@ -469,11 +427,11 @@ func main() {
 
 		if len(Device_Client) != 0 {
 			go SensorBatchInsert(result.Batchsize)
-			// for index, device := range Device_Client {
-			// 	ticker := time.NewTicker(300 * time.Millisecond)
-			// 	wg.Add(1)
-			// 	go ReadModbus(index, device, 1300, 125, &wg ,result.Count, result.Batchsize, ticker)
-			// }
+			for index, device := range Device_Client {
+				ticker := time.NewTicker(300 * time.Millisecond)
+				wg.Add(1)
+				go ReadModbus(index, device, 1300, 125, &wg ,result.Count, result.Batchsize, ticker)
+			}
 		} else {
 			log.Fatal("연결된 디바이스가 없습니다.")
 			return c.SendString("연결된 디바이스가 없습니다")
